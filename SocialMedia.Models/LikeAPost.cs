1239b692ef7036613934c46725c460889e626658
aaroncarnahan <aaron.b.carnahan@gmail.com>
2020-11-13 16:52:33 -0500

    aaron's next commit

---
+++ SocialMedia.Models/LikeAPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+	public class LikeAPost
+	{
+		[Required]
+		public bool Liked { get; set; }
+		public int PostId { get; set; }
+	}
+}
--- SocialMedia.Services/LikeService.cs
+++ SocialMedia.Services/LikeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,50 @@ using System.Threading.Tasks;
 
 namespace SocialMedia.Services
 {
-    class LikeService
+    public class LikeService
     {
+        private readonly Guid _userId;
+        public LikeService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+		// Posting a comment
+
+		public bool LikeAPost(LikeAPost model)
+		{
+			var entity = new Like()
+			{
+				Liker = _userId,
+				PostId = model.PostId
+			};
+
+			using (var ctx = new ApplicationDbContext())
+			{
+				ctx.Likes.Add(entity);
+				return ctx.SaveChanges() == 1;
+			}
+		}
+
+
+
+		//// Get comment
+
+		//public IEnumerable<GetPostComments> GetComment()
+  //      {
+  //          using (var ctx = new ApplicationDbContext())
+  //          {
+  //              var query =
+  //                  ctx
+  //                      .Posts
+  //                      .Where(e => e.Author == _userId)
+  //                      .Select(e => new GetPostComments
+  //                      {
+  //                          Id = e.Id,
+  //                          Text = e.Text
+  //                      });
+  //              return query.ToArray();
+  //          }
+  //      }
     }
 }
